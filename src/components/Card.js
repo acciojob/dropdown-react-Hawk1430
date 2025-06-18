845e4f57557fac3fabd000dc0d6d3482408ccea3
Prashant Raj <pr2125116@gmail.com>
2025-06-18 14:06:55 +0530

    Done

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Card = ({id,name, description}) => {
   return (
     <div>
-        <div id='state-name'>
+        <div id='state-title'>
             <h2>{name}</h2>
         </div>
         <div id='state-decription'>
